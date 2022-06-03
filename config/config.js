a350148009c85639fa100327ba32bac17354d856
faprayyy <fauzanjr1@gmail.com>
2022-06-03 16:28:16 +0700

    KEMBALI KE SEMULA SAJA

--- config/config.js
+++ config/config.js
@@ -15,12 +15,12 @@ module.exports = {
         dialect: "postgres"
     },
     production: {
-        username: "pfzqxocydqvswt",
+        username: "nnmmtewomaqhls",
         password: "13db1dc4371c19e7c780bec629b40da515c49865049e82f5055e01e470505946",
-        database: "d3juovg27i85ot",
-        host: "ec2-52-4-104-184.compute-1.amazonaws.com",
+        database: "d2b70f2ujk5bjs",
+        host: "ec2-34-231-221-151.compute-1.amazonaws.com",
         port: 5432,
-        url: `postgres://pfzqxocydqvswt:13db1dc4371c19e7c780bec629b40da515c49865049e82f5055e01e470505946@ec2-52-4-104-184.compute-1.amazonaws.com:5432/d3juovg27i85ot`,
+        url: `postgres://nnmmtewomaqhls:c48e3b6fae673e06dfbefb54a1167aece2d03bb34abdeb2495bc2d7693dad791@ec2-34-231-221-151.compute-1.amazonaws.com:5432/d2b70f2ujk5bjs`,
         dialect: "postgres",
         dialectOptions: {
           ssl: { 
