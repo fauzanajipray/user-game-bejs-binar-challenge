a994f4cecacbb3816e5c8a09c1f72042a80dc69d
faprayyy <fauzanjr1@gmail.com>
2022-06-03 16:47:58 +0700

    update password

--- config/config.js
+++ config/config.js
@@ -16,11 +16,11 @@ module.exports = {
     },
     production: {
         username: "nnmmtewomaqhls",
-        password: "13db1dc4371c19e7c780bec629b40da515c49865049e82f5055e01e470505946",
+        password: "c48e3b6fae673e06dfbefb54a1167aece2d03bb34abdeb2495bc2d7693dad791",
         database: "d2b70f2ujk5bjs",
         host: "ec2-34-231-221-151.compute-1.amazonaws.com",
         port: 5432,
-        url: `postgres://nnmmtewomaqhls:c48e3b6fae673e06dfbefb54a1167aece2d03bb34abdeb2495bc2d7693dad791@ec2-34-231-221-151.compute-1.amazonaws.com:5432/d2b70f2ujk5bjs`,
+        url: "postgres://nnmmtewomaqhls:c48e3b6fae673e06dfbefb54a1167aece2d03bb34abdeb2495bc2d7693dad791@ec2-34-231-221-151.compute-1.amazonaws.com:5432/d2b70f2ujk5bjs",
         dialect: "postgres",
         dialectOptions: {
           ssl: { 
