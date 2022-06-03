268e006454fba384f2ded7da71c711e56208a3aa
faprayyy <fauzanjr1@gmail.com>
2022-06-03 16:24:49 +0700

    KEMBALI KE SEMUJLA

--- config/config.js
+++ config/config.js
@@ -15,13 +15,13 @@ module.exports = {
         dialect: "postgres"
     },
     production: {
-        username: process.env.PROD_DB_USERNAME,
-        password: process.env.PROD_DB_PASSWORD,
-        database: process.env.PROD_DB_NAME,
-        host: process.env.PROD_DB_HOSTNAME,
-        port: parseInt(process.env.PROD_DB_PORT),
-        url: `${process.env.PROD_DB_DIALECT}://${process.env.PROD_DB_USERNAME}:${process.env.PROD_DB_PASSWORD}@${process.env.PROD_DB_HOSTNAME}:${process.env.PROD_DB_PORT}/${process.env.PROD_DB_NAME}`,
-        dialect: process.env.PROD_DB_DIALECT,
+        username: "pfzqxocydqvswt",
+        password: "13db1dc4371c19e7c780bec629b40da515c49865049e82f5055e01e470505946",
+        database: "d3juovg27i85ot",
+        host: "ec2-52-4-104-184.compute-1.amazonaws.com",
+        port: 5432,
+        url: `postgres://pfzqxocydqvswt:13db1dc4371c19e7c780bec629b40da515c49865049e82f5055e01e470505946@ec2-52-4-104-184.compute-1.amazonaws.com:5432/d3juovg27i85ot`,
+        dialect: "postgres",
         dialectOptions: {
           ssl: { 
             require: true,
